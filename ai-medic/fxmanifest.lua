0a32579714f933ad21e40427bfc29a92a598f0c1
johnny-development <bananarenan5@gmail.com>
2024-12-20 09:35:31 +1100

    Update fxmanifest.lua

--- ai-medic/fxmanifest.lua
+++ ai-medic/fxmanifest.lua
@@ -1,15 +1,23 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Johnny'
+author 'Dingos Development :: Developer: Johnny'
 description 'AI Medic Script for QBCore'
 version '1.0.0'
 
+-- Shared configuration
+shared_script 'config.lua'
+
+-- Client-side scripts
 client_scripts {
     'client.lua'
 }
 
+-- Server-side scripts
 server_scripts {
     '@qb-core/server/main.lua',
     'server.lua'
 }
+
+-- For custom scripts join the link below your first order is on us
+-- https://discord.gg/mQQ2D28XqK
